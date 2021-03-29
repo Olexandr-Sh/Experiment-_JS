987367457676a9f69df074a93c416d6c94ce1d3c
Olexandr-Sh <shevtcov07@ukr.net>
2021-03-29 21:34:57 +0300

    added button with JS function

--- index.js
+++ index.js
@@ -2,4 +2,18 @@ const para = document.querySelector('p');
 para.addEventListener('click', updateName);
 function updateName() {
   let name = prompt('Enter a new name');
-  para.textContent = 'Player 1: ' + name;}
+  para.textContent = 'Player 1: ' + name;
+}
+document.addEventListener("DOMContentLoaded", function() {
+  function createParagraph() {
+    let para = document.createElement('p');
+    para.textContent = 'You clicked the button!';
+    document.body.appendChild(para);
+  }
+
+  const buttons = document.querySelectorAll('button');
+
+  for(let i = 0; i < buttons.length ; i++) {
+    buttons[i].addEventListener('click', createParagraph);
+  }
+});
